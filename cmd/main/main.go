e3df79b8ba8e7bd52fb4053578e454ed0fb215ab
Qussai Khalil <qussaikhalil@icloud.com>
2025-05-30 02:08:57 +0200

    prepere for render

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,14 +3,23 @@ package main
 import(
 	"log"
 	"net/http"
+	"os"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/qussaikh/go-bookstore/pkg/routes"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+	// Get port from environment (Render sets this)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9010" // fallback for local dev
+	}
+
+	log.Printf("Server starting on port %s...\n", port)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, r))
 }
